14fe2e173570994bb78570f53f1d73e7fab148c3
Subham Kumar Mallick <83875827+sk-mallick@users.noreply.github.com>
2026-02-10 07:19:59 +0000

    update engine type from "mcq" to "fill" in middle and primary config files

--- data/sva/middle/config.js
+++ data/sva/middle/config.js
@@ -2,6 +2,6 @@ export default {
     order: 4,                    // The number shown on the card (01, 02...)
     title: "Subject-Verb Agreement",  // The big bold title
     description: "4 Sets • 15 Questions Each - M", // The small text below
-    engine: "mcq",               // "mcq" or "fill" (which HTML file to open)
+    engine: "fill",               // "mcq" or "fill" (which HTML file to open)
     icon: "list"                 // "time", "list", "book", or "chat"
 }
--- data/sva/primary/config.js
+++ data/sva/primary/config.js
@@ -2,6 +2,6 @@ export default {
     order: 3,                    // The number shown on the card (01, 02...)
     title: "Subject-Verb Agreement",  // The big bold title
     description: "13 Sets • 15 Questions Each - P", // The small text below
-    engine: "mcq",               // "mcq" or "fill" (which HTML file to open)
+    engine: "fill",               // "mcq" or "fill" (which HTML file to open)
     icon: "list"                 // "time", "list", "book", or "chat"
 }
