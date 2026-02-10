b22da21a647b3f649bda6835a2f28ca09d974221
Subham Kumar Mallick <83875827+sk-mallick@users.noreply.github.com>
2026-02-10 04:18:33 +0000

    delete files

--- data/sva/high/config.js
+++
@@ -1,7 +0,0 @@
-export default {
-    order: 1,                    // The number shown on the card (01, 02...)
-    title: "Tenses (Advanced)",  // The big bold title
-    description: "MCQ • High Level", // The small text below
-    engine: "mcq",               // "mcq" or "fill" (which HTML file to open)
-    icon: "time"                 // "time", "list", "book", or "chat"
-}
